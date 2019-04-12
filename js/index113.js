f53e34312e32454ed36e397129ee8363eaa34b52
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-04-12 17:46:57 +0300

    changed exercise 11.1

--- js/index113.js
+++ js/index113.js
@@ -1,46 +1,45 @@
 
 
-	var messages = [
-	    "backspace",
-	    "enter",
-	    "shift",
-	    "control",
-	    "delete",
-	    "space",
-	    "subtract"
-	]
+var messages = [
+"backspace",
+"enter",
+"shift",
+"control",
+"delete",
+"space",
+"subtract"
+]
 
-	messages.getKey = () => new Date().toLocaleString().split(", ")[1]
+messages.getKey = () => new Date().toLocaleString().split(", ")[1]
 
-	var log = {}
+var log = {}
 
-	var sendMessage = message => new Promise (
-	    resolve => setTimeout (
-	        () => resolve ( message ),
-	        Math.random () * 5000
-	    )
+var sendMessage = message => new Promise (
+	resolve => setTimeout (
+		() => resolve ( message ),
+		Math.random () * 5000
+		)
 	)
 
-	var sendAll = () => {
-	    var index = 0
-	    function recursive () {
-	        sendMessage ( messages [ index++ ] )
-	            .then (
-	                response => {
-	                    if ( !response ) return
-	                    Object.assign ( log,
-	                        { [ messages.getKey() ] : response }
-	                    )
-	            		recursive ()
-	            	}
-				) 
-	    }
-	    recursive ()
+var sendAll = () => {
+	var index = 0
+	function recursive () {
+		sendMessage ( messages [ index++ ] )
+		.then (
+			response => {
+				if ( !response ) return
+					Object.assign ( log,
+						{ [ messages.getKey() ] : response }
+						)
+				recursive ()
+			}
+			) 
+	}
+	recursive ()
 }
+sendAll()
 
-	sendAll()
 
-	// console.log (log)
 function exercise3 (event) {
 	console.log (log)
 }
